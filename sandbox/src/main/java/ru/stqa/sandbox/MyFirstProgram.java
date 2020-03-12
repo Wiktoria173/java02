7a02cda4cbf4dc489bcdcb1f18926ad923589bab
ja <ika.stw@gmail.com>
2020-03-12 20:54:58 +0100

    Add methods

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -7,22 +7,14 @@ public class MyFirstProgram {
     hello("user");
 
     Square s = new Square(5); // tu zostaje swtorzony obieky typu s
-    System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + area(s)); //tu: przenosze obiekt w charakterze parametru w funkcje area
+    System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + s.area()); //tu: przenosze obiekt w charakterze parametru w funkcje area
 
     Rectangle r = new Rectangle(4, 6);
-    System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + area(r));
+    System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
   }
 
 }
