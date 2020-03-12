f191d0be01192800b8c426782ef96824a9657146
ja <ika.stw@gmail.com>
2020-03-12 20:23:39 +0100

    Add a few functions

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -3,7 +3,27 @@ package ru.stqa.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    hello("world");
+    hello("user");
+
+    double l = 5;
+    System.out.println("Powierzchnia kwadratu o boku " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
