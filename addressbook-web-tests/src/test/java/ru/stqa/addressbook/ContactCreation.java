0620432bc1461c59997356400a6cd20e86ecf64a
ja <ika.stw@gmail.com>
2020-03-17 20:07:11 +0100

    ContactCreation - Add parameters to methods

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/ContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/ContactCreation.java
@@ -1,12 +1,9 @@
 package ru.stqa.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class ContactCreation {
   private WebDriver wd;
@@ -16,23 +13,23 @@ public class ContactCreation {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testContactCreation() throws Exception {
     initContactCreation();
-    fillContactForm();
+    fillContactForm("John", "Cornell", "789456123", "john@cornell.com");
     submitContactCreation();
     returnToHomePage();
   }
@@ -40,24 +37,24 @@ public class ContactCreation {
   private void returnToHomePage() {
     wd.findElement(By.linkText("home page")).click();
   }
- 
+
   private void submitContactCreation() {
     wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
   }
 
-  private void fillContactForm() {
+  private void fillContactForm(String firstname, String lastname, String telhome, String email) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("John");
+    wd.findElement(By.name("firstname")).sendKeys(firstname);
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Cornell");
+    wd.findElement(By.name("lastname")).sendKeys(lastname);
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("789456123");
+    wd.findElement(By.name("home")).sendKeys(telhome);
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("john@cornell.com");
+    wd.findElement(By.name("email")).sendKeys(email);
   }
 
   private void initContactCreation() {
