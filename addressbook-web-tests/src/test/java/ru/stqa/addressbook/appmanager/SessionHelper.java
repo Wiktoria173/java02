3a6cde81297f48d42cf37ad83e207688c5e085ee
ja <ika.stw@gmail.com>
2020-04-04 21:17:43 +0200

    Add method login to SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/SessionHelper.java
@@ -1,5 +1,6 @@
 package ru.stqa.addressbook.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SessionHelper {
@@ -9,5 +10,14 @@ public class SessionHelper {
   public SessionHelper(WebDriver wd) {
 
     this.wd = wd;
+  }
+  public void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 }
