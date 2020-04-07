f3e51ac89b7192b4cbadcd893ab3054417bfbba2
ja <ika.stw@gmail.com>
2020-04-07 21:55:48 +0200

    Update Equation class

--- sandbox/src/main/java/ru/stqa/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/sandbox/Equation.java
@@ -16,29 +16,24 @@ public class Equation {
 
     double d = b*b - 4*a*c;
 
-    if (a == 0) {
-      if (b == 0) {
-        if (c == 0) {
-          n = -1;
-        } else {
-          n = 0;
-        }
-      } else {
-        n = 1;
-      }
-
-    } else {
+    if (a != 0) {
       if (d > 0) {
         n = 2;
+      } else if (d == 0) {
+        n = 1;
       } else {
-        if (d == 0) {
-          n = 1;
-        } else {
-          n = 0;
-        }
+        n = 0;
       }
-    }
 
+    } else if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n = -1;
+    }
 
   }
 
@@ -46,3 +41,4 @@ public class Equation {
     return n;
   }
 }
+
