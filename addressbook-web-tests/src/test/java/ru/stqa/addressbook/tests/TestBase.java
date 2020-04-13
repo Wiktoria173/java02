04ca3cbf874f14aac2db4a91d926c48c79d17049
ja <ika.stw@gmail.com>
2020-04-13 10:18:45 +0200

    Start tests in different browsers

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -5,10 +5,11 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.addressbook.appmanager.ApplicationManager;
 import ru.stqa.addressbook.model.ContactData;
+import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
