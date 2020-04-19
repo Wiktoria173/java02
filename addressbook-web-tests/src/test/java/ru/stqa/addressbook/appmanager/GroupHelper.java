8ff52d76f1e7c0dc271118ffb0018be77c9ee76e
ja <ika.stw@gmail.com>
2020-04-19 17:45:53 +0200

    Checking and ensuring that preconditions have been carried out for group tests

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/GroupHelper.java
@@ -43,4 +43,14 @@ public class GroupHelper extends HelperBase {
   }
 
 
+  public void createGroup(GroupData group) {
+    initGroupCreation();
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  public boolean isThereAGroup() {
+    return isElementPresent(By.name("selected[]"));
+  }
 }
